14a8719ef8a2479a43bed03fb2e40cb269df484f
Tom <tommygoggles@gmail.com>
2018-01-08 20:55:28 +0000

    Update make TGSNES.bat

--- make TGSNES.bat
+++ make TGSNES.bat
@@ -1,4 +1,4 @@
 @echo off 
 "wla-dx-9.5-win32-bin-2013-SVN-rev8-WJ/wla-65816" -vo TGSNES.asm TGSNES.obj
 "wla-dx-9.5-win32-bin-2013-SVN-rev8-WJ/wlalink" -vr TGSNES.link TGSNES.smc
-"snes9x1.51.ep10r2/Snes9X1.51.ep10r2"
+"snes9x1.51.ep10r2/Snes9X1.51.ep10r2" TGSNES.smc
